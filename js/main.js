79f0d43599f123b3894a8b0356adb0ad01ca1e41
Ivan <work.gorobec@gmail.com>
2021-06-06 16:46:29 +0500

    Поменял имена переменных, сделал все функции стрелочными, переписал функцию случайного массива

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const TYPE_LIST = [
+const OFFER_TYPES = [
   'palace',
   'flat',
   'house',
@@ -6,13 +6,13 @@ const TYPE_LIST = [
   'hotel',
 ];
 
-const CHECK_LIST = [
+const CHECKINS = [
   '12:00',
   '13:00',
   '14:00',
 ];
 
-const FEATURES_LIST = [
+const FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -21,65 +21,50 @@ const FEATURES_LIST = [
   'conditioner',
 ];
 
-const PHOTOS_LIST = [
+const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
 ];
 
-function getRandomFloat (min, max, precision) {
+const getRandomFloat = (min, max, precision) => {
   if (max <= min || max < 0 || min < 0) {
     return null;
   }
   const result = (Math.random() * (max - min + 0.1)) + min;
   return Number(result.toFixed(precision));
-}
+};
 
-function getRandomInteger (min, max) {
-  return getRandomFloat(min, max, 0);
-}
+const getRandomInteger = (min, max) => getRandomFloat(min, max, 0);
 
-function getRandomArrayElement (array) {
-  return array[getRandomInteger(0, array.length - 1)];
-}
+const getRandomArrayElement = (array) => array[getRandomInteger(0, array.length - 1)];
 
-function getRandomArray (array) {
-  const randomArray = new Array(getRandomInteger(0, array.length - 1));
-  let index = 0;
-  while (index <= randomArray.length - 1) {
-    const value = array[getRandomInteger(0, array.length - 1)];
-    if(!randomArray.includes(value)) {
-      randomArray[index] = value;
-      index++;
-    }
-  }
-  return randomArray;
-}
+const getRandomArray = (array) => {
+  const newArray = array.slice().sort(() => Math.random() - 0.5);
+  return newArray.slice(0, getRandomInteger(0, newArray.length - 1));
+};
 
-function createAdvert() {
-  return {
-    author: {
-      avatar: `img/avatars/user/0${getRandomInteger(1,8)}.png`,     //<---- я не понял как сделать так чтоб адреса не повторялись..объектов нужно 10 по заданию,
-    },                                                              //а адресов получается только 8 (число нужно рандомить от 1 до 8)
-    offer: {
-      title: 'Do you need a room?',
-      address: `${location.x}, ${location.y}`,
-      price: getRandomInteger(1, 5000),
-      type: getRandomArrayElement(TYPE_LIST),
-      rooms: getRandomInteger(0, 5),
-      guests: getRandomInteger(0, 5),
-      checkin: getRandomArrayElement(CHECK_LIST),
-      checkout: getRandomArrayElement(CHECK_LIST),
-      features: getRandomArray(FEATURES_LIST),
-      description: 'Сдаётся комната в центре Токио, все удобства',
-      photos: getRandomArray(PHOTOS_LIST),
-    },
-    location: {
-      lat: getRandomFloat(35.65000, 35.70000, 5),
-      lng: getRandomFloat(139.70000, 139.80000, 5),
-    },
-  };
-}
-
+const createAdvert = () => ({
+  author: {
+    avatar: `img/avatars/user/0${getRandomInteger(1,8)}.png`,
+  },
+  offer: {
+    title: 'Do you need a room?',
+    address: `${location.x}, ${location.y}`,
+    price: getRandomInteger(1, 5000),
+    type: getRandomArrayElement(OFFER_TYPES),
+    rooms: getRandomInteger(0, 5),
+    guests: getRandomInteger(0, 5),
+    checkin: getRandomArrayElement(CHECKINS),
+    checkout: getRandomArrayElement(CHECKINS),
+    features: getRandomArray(FEATURES),
+    description: 'Сдаётся комната в центре Токио, все удобства',
+    photos: getRandomArray(PHOTOS),
+  },
+  location: {
+    lat: getRandomFloat(35.65000, 35.70000, 5),
+    lng: getRandomFloat(139.70000, 139.80000, 5),
+  },
+});
+// eslint-disable-next-line no-unused-vars
 const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
-similarAdverts;
