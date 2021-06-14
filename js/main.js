c5dbdb09857fcc0d0c8b1ba2374153fbfd9c97e9
Ivan <work.gorobec@gmail.com>
2021-06-14 12:59:20 +0500

    Добавил код для создания похожих объявлений

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-import './data.js';
+import {createAdvert} from './data.js';
+
+// eslint-disable-next-line no-unused-vars
+const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
