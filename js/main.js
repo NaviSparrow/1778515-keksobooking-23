92f17d223dda9f7b199277f5302a121e43a0da76
Ivan <work.gorobec@gmail.com>
2021-06-19 00:42:34 +0500

    подключил модуль для генерации разметки похожих объявлений

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import {createAdvert} from './data.js';
+import './popup.js';
 
-// eslint-disable-next-line no-unused-vars
-const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
+// // eslint-disable-next-line no-unused-vars
+// const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
