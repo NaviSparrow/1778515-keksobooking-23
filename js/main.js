50f714ce02d27ccd6892e6f270a4ef83d275659b
Ivan <work.gorobec@gmail.com>
2021-06-20 17:25:50 +0500

    убрал фрагменты, переименновал функции, поправил аргументы

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 import {createAdvert} from './data.js';
-import {createPopup} from './popup.js';
-import {mapCanvas} from './popup.js';
+import {showPopup} from './popup.js';
 
-const advertsPopupFragment = document.createDocumentFragment();
+
 const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
-similarAdverts.forEach((advert) => {
-  advertsPopupFragment.appendChild(createPopup(advert));
-});
-
-mapCanvas.appendChild(advertsPopupFragment.firstChild);
+showPopup(similarAdverts[0]);
