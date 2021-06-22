e5a6a587d2e58e3081f04ee804759cb95981364d
Ivan <work.gorobec@gmail.com>
2021-06-23 00:53:13 +0500

    убрал из модуля form лишние переменные

--- js/form.js
+++ js/form.js
@@ -1,30 +1,4 @@
 const noticeForm = document.querySelector('.ad-form');
-const formElements = document.querySelectorAll('.ad-form__element');
-const mapFiltersForm = document.querySelector('.map__filters');
-const mapFiltersElements = document.querySelectorAll('.map__filter');
+const noticeFormElements = document.querySelectorAll('.ad-form__element');
 
-const setFormDisabled = (form, elements) => {
-  form.classList.add('ad-form--disabled');
-  for (const element of elements) {
-    element.setAttribute('disabled', 'disabled');
-  }
-};
-
-const setFormEnabled = (form, elements) => {
-  form.classList.remove('ad-form--disabled');
-  for (const element of elements) {
-    element.removeAttribute('disabled', 'disabled');
-  }
-};
-
-const getActivePage = () => {
-  setFormEnabled(noticeForm, formElements);
-  setFormEnabled(mapFiltersForm, mapFiltersElements);
-};
-
-const getNotActivePage = () => {
-  setFormDisabled(noticeForm, formElements);
-  setFormDisabled(mapFiltersForm, mapFiltersElements);
-};
-
-export {getActivePage, getNotActivePage};
+export {noticeForm, noticeFormElements};
