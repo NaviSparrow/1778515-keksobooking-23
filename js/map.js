90659811fcd2fb27de96d936f7254a6f415011d1
Ivan <work.gorobec@gmail.com>
2021-06-23 00:50:37 +0500

    создал модуль map для работы с картой

---
+++ js/map.js
@@ -0,0 +1,4 @@
+const mapFiltersForm = document.querySelector('.map__filters');
+const mapFiltersElements = document.querySelectorAll('.map__filter');
+
+export {mapFiltersForm, mapFiltersElements};
