d02adc72dcd61cbabfd8d7291aa3402d14140f43
Ivan <work.gorobec@gmail.com>
2021-06-30 00:15:45 +0500

    убрал "магическое значение" в модуле form,  заинлайнил функцию в main

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 import {createAdvert} from './data.js';
 import {setPageEnabled} from './page.js';
-import { createMarker } from './map.js';
-import { setOnMapLoad } from './map.js';
+import {createMarker} from './map.js';
+import {setOnMapLoad} from './map.js';
 import './form.js';
 
 setPageEnabled(false);
 
-const createItemsForMap = () => {
+setOnMapLoad(() => {
   setPageEnabled(true);
   const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
   similarAdverts.forEach((advert) => createMarker(advert));
-};
+});
 
-setOnMapLoad(createItemsForMap);
