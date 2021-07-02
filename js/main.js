b1bce569a124c546189a4d70df3e30158c08cc36
Ivan <work.gorobec@gmail.com>
2021-07-03 02:24:09 +0500

    добавил функцию для действий при отправке формы

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-import {createAdvert} from './data.js';
 import {setPageEnabled} from './page.js';
-import { createMarker } from './map.js';
+import { createMarkers } from './map.js';
 import { setOnMapLoad } from './map.js';
+import { getData } from './fetch.js';
+import { setOnFormSubmit } from './form.js';
+import { showSuccsess } from './popup.js';
 import './form.js';
 
 setPageEnabled(false);
 
 setOnMapLoad(() => {
   setPageEnabled(true);
-  const similarAdverts = new Array(10).fill(null).map(() => createAdvert());
-  similarAdverts.forEach((advert) => createMarker(advert));
+  getData(createMarkers);
 });
+
+setOnFormSubmit(showSuccsess);
